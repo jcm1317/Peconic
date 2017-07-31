77db7b4e7be94d56b74b5ec9d50de1ab22889f65
Jared Miller <jaredmiller@twitter.com>
2017-07-30 19:37:59 -0700

    Change divs to flex display and remove resizing js.

--- js/albums.js
+++ js/albums.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
-  // Set infoBox width to size of album art
-  var imgWidth = $('.album-art').width();
-  $('.infoBox').width(imgWidth);
   $('.nav').affix({offset: {top: 200} });
 });
 
-$( window ).resize(function() {
-  var imgWidth = $('.album-art').width();
-  $('.infoBox').width(imgWidth);
-});
